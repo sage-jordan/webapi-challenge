bb8d1076cb34158df041a6097cc00a4960343d5e
sage-jordan <sagemjrodan@gmail.com>
2019-11-10 22:13:03 -0500

    project PUT and DELETE

--- data/api/projectCRUD.js
+++ data/api/projectCRUD.js
@@ -39,6 +39,29 @@ router.post('/', (req, res) => {
 
 // PUT 
 
+router.put('/:id', (req, res) => {
+    const id = req.params.id;
+    const changes = req.body;
+    db.update(id, changes)
+        .then(newProject => {
+            res.status(200).json({ success: true, newProject });
+        })
+        .catch( err => {
+            res.status(500).json({ success: false, err });
+        });
+})
+
 // DELETE
+
+router.delete('/:id', (req, res) => {
+    const id = req.params.id;
+    db.remove(id)
+        .then(() => {
+            res.status(204).json({ success: true, message: `Project with id ${id} has successfully been deleted.`});
+        })
+        .catch(err => {
+            res.status(500).json({ success: false, err });
+        });
+})
 
 module.exports = router;
