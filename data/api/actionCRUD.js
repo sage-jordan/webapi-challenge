6448e7faeeb52a17c4c762368f719be701a83b91
sage-jordan <sagemjrodan@gmail.com>
2019-11-10 22:03:37 -0500

    GET PROJECT/ID

--- data/api/actionCRUD.js
+++ data/api/actionCRUD.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const router = express.Router();
 
-const adb = require('../helpers/actionModel.js');
+const db = require('../helpers/actionModel.js');
 
 
 // ACTION CRUD
--- data/api/projectCRUD.js
+++ data/api/projectCRUD.js
@@ -2,12 +2,27 @@ const express = require('express');
 
 const router = express.Router();
 
-const pdb = require('../helpers/projectModel');
+const db = require('../helpers/projectModel');
 
 
 // PROJECT CRUD
 
 // GET
+
+router.get('/:id', (req, res) => {
+    const id = req.params.id;
+    db.get(id)
+        .then(project => {
+            if(project){
+                res.status(200).json({ success: true, project });
+            } else {
+                res.status(404).json({ success: false, message: `could not find a prject with id: ${id}` });
+            }
+        })
+        .catch(err => {
+            res.status(500).json({ success: false, err });
+        })
+});
 
 // POST
 
